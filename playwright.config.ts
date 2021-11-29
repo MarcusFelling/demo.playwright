6d75acf6558bcbce1b14f99c07f62a3f389b312a
Marcus Felling <MarcusFelling@microsoft.com>
2021-11-29 12:52:22 -0600

    Use root config

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,18 +6,11 @@ const config: PlaywrightTestConfig = {
   // Timeout per test
   timeout: 30 * 1000,
   // Test directory
-  testDir: path.join(__dirname, 'e2e'),
+  testDir: path.join(__dirname, 'e2e-examples'),
   // If a test fails on CI, retry it additional 2 times
   retries: process.env.CI ? 2 : 0,
   // Artifacts folder where screenshots, videos, and traces are stored.
   outputDir: 'test-results/',
-
-  // Run your local dev server before starting the tests:
-  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
 
   use: {
     // Run headless by default
