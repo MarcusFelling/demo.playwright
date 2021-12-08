0c4c615497355ad310a99918fbf1acd0e379370c
Marcus Felling <MarcusFelling@microsoft.com>
2021-12-08 16:42:54 -0600

    always trace

--- e2e-examples/playwright.config.ts
+++ e2e-examples/playwright.config.ts
@@ -44,7 +44,7 @@ const config: PlaywrightTestConfig = {
     actionTimeout: 0,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
