81eb40d9ff0a265b8e83f660abcb8ffa265193be
Marcus Felling <MarcusFelling@microsoft.com>
2021-12-29 11:35:52 -0600

    try runner

---
+++ android/example.spec.js
@@ -0,0 +1,48 @@
+import { _android as android } from '@playwright/test';
+
+(async () => {
+  // Connect to the device.
+  const [device] = await android.devices();
+  console.log(`Model: ${device.model()}`);
+  console.log(`Serial: ${device.serial()}`);
+  // Take screenshot of the whole device.
+  await device.screenshot({ path: 'device.png' });
+
+  {
+    // --------------------- WebView -----------------------
+
+    // Launch an application with WebView.
+    await device.shell('am force-stop org.chromium.webview_shell');
+    await device.shell('am start org.chromium.webview_shell/.WebViewBrowserActivity');
+    // Get the WebView.
+    const webview = await device.webView({ pkg: 'org.chromium.webview_shell' });
+
+    // Fill the input box.
+    await device.fill({ res: 'org.chromium.webview_shell:id/url_field' }, 'github.com/microsoft/playwright');
+    await device.press({ res: 'org.chromium.webview_shell:id/url_field' }, 'Enter');
+
+    // Work with WebView's page as usual.
+    const page = await webview.page();
+    await page.waitForNavigation({ url: /.*microsoft\/playwright.*/ });
+    console.log(await page.title());
+  }
+
+  {
+    // --------------------- Browser -----------------------
+
+    // Launch Chrome browser.
+    await device.shell('am force-stop com.android.chrome');
+    const context = await device.launchBrowser();
+
+    // Use BrowserContext as usual.
+    const page = await context.newPage();
+    await page.goto('https://webkit.org/');
+    console.log(await page.evaluate(() => window.location.href));
+    await page.screenshot({ path: 'page.png' });
+
+    await context.close();
+  }
+
+  // Close the device.
+  await device.close();
+})();
