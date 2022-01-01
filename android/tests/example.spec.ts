37247777f44554c154f0e3065833a9781e18a381
Marcus Felling <MarcusFelling@microsoft.com>
2021-12-31 19:01:36 -0600

    remove chrome tests

--- android/tests/example.spec.ts
+++ android/tests/example.spec.ts
@@ -31,28 +31,4 @@ test.describe('Run native Android tests', async () => {
     // Close the device.
     await device.close();    
   });
-  
-  test('Chrome Browser', async () => {
-    // Connect to the device.
-    const [device] = await android.devices();
-    console.log(`Model: ${device.model()}`);
-    console.log(`Serial: ${device.serial()}`);
-    // Take screenshot of the whole device.
-    await device.screenshot({ path: 'device.png' });
-
-    // Launch Chrome browser.
-    await device.shell('am force-stop com.android.chrome');
-    const context = await device.launchBrowser();
-
-    // Use BrowserContext as usual.
-    const page = await context.newPage();
-    await page.goto('https://webkit.org/');
-    console.log(await page.evaluate(() => window.location.href));
-    await page.screenshot({ path: 'page.png' });
-
-    await context.close();
-
-    // Close the device.
-    await device.close();    
-  });
 });
