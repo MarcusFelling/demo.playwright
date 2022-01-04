9620852fad644bf153d43ce91046ca4c351fad85
Marcus Felling <MarcusFelling@microsoft.com>
2022-01-04 15:33:28 -0600

    chromium

--- oauth/playwright.config.ts
+++ oauth/playwright.config.ts
@@ -49,6 +49,12 @@ const config: PlaywrightTestConfig = {
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox'],
