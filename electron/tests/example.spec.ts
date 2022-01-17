30e37869265608f8f43ac07368b4b614726593f8
Marcus Felling <MarcusFelling@microsoft.com>
2022-01-17 10:20:57 -0600

    add afterEach to close, add assertions

--- electron/tests/example.spec.ts
+++ electron/tests/example.spec.ts
@@ -2,28 +2,34 @@
  * Example automation for electron
  * @see https://playwright.dev/docs/api/class-electron
  */
-import { _electron as electron, test } from '@playwright/test';
+ import { _electron as electron, test, expect, ElectronApplication } from '@playwright/test';
 
-test('Run electron tests', async () => {
-  // Launch Electron app.
-  const electronApp = await electron.launch({ args: ['dist/index.js'] });
-
-  // Evaluation expression in the Electron context.
-  const appPath = await electronApp.evaluate(async ({ app }) => {
-    // This runs in the main Electron process, parameter here is always
-    // the result of the require('electron') in the main app script.
-    return app.getAppPath();
-  });
-  console.log(appPath);
-
-  // Get the first window that the app opens, wait if necessary.
-  const window = await electronApp.firstWindow();
-  // Print the title.
-  console.log(await window.title());
-  // Capture a screenshot.
-  await window.screenshot({ path: 'intro.png' });
-  // Direct Electron console to Node terminal.
-  window.on('console', console.log);
-  // Exit app.
-  await electronApp.close();  
-});
+ let electronApp: ElectronApplication;
+ 
+ test.beforeEach(async () => {
+   // Launch Electron app.
+   electronApp = await electron.launch({ args: ['dist/index.js'] });
+ });
+ 
+ test.afterEach(async () => {
+   // Launch Electron app.
+   await electronApp.close();
+ });
+ 
+ test('app path', async () => {
+ 
+   // Evaluation expression in the Electron context.
+   const appPath = await electronApp.evaluate(async ({ app }) => {
+     // This runs in the main Electron process, parameter here is always
+     // the result of the require('electron') in the main app script.
+     return app.getAppPath();
+   });
+   expect(appPath).toContain('electron/dist');
+ });
+ 
+ test('app title', async () => {
+   // Get the first window that the app opens, wait if necessary.
+   const window = await electronApp.firstWindow();
+   // Print the title.
+   await expect(window).toHaveTitle('Hello World!');
+ });
