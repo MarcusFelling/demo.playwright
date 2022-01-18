15348a2a506a505f3ad6399ccfeb170cf8036656
Marcin3 <marcinszafranek@gmail.com>
2022-01-18 17:12:40 +0100

    Add path to ToDoMVC site

--- todomvc/tests/example.spec.ts
+++ todomvc/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('');
+  await page.goto('https://demo.playwright.dev/todomvc');
 });
 
 const TODO_ITEMS = [
