29a3d69d1c754a1c4ee2862fcb09ef10d772da99
Marcus Felling <>
2022-01-19 11:11:45 -0600

    use code from docs

--- chrome-extension/tests/example.spec.ts
+++ chrome-extension/tests/example.spec.ts
@@ -3,35 +3,30 @@
    * of an extension whose source is located in ./my-extension
    * @see https://playwright.dev/docs/chrome-extensions
   */ 
- import { test, chromium, expect, BrowserContext } from '@playwright/test';
+import { test, chromium, BrowserContext } from '@playwright/test';
 
- test.describe('chrome extension tests', () => { 
-   let browserContext: BrowserContext;
- 
-   test.beforeEach(async ({}, testInfo) => {
-     const pathToExtension = require('path').join(__dirname, '../my-extension');
-     const userDataDir = testInfo.outputPath('test-user-data-dir');
-     browserContext = await chromium.launchPersistentContext(userDataDir, {
-       args: [
-         `--disable-extensions-except=${pathToExtension}`,
-         `--load-extension=${pathToExtension}`
-       ]
-     });
-   });
- 
-   test.afterEach(async () => {
-     // Don't forget to close the created context.
-     // Closing persistent context will close the browser.
-     await browserContext.close();
-   });
- 
-   test('Should get handle to background page of extension', async ({}, testInfo) => {
-     // Wait for background page if it is not yet loaded.
-     let [backgroundPage] = browserContext.backgroundPages();
-     if (!backgroundPage)
-       backgroundPage = await browserContext.waitForEvent('backgroundpage');
- 
-     expect(backgroundPage).toBeTruthy();
-     // Test the background page as you would any other page.
-   });
- });
+test.describe('chrome extension tests', () => { 
+  let browserContext: BrowserContext;
+
+  test.beforeEach(async ({}, testInfo) => {
+    const pathToExtension = require('path').join(__dirname, '../my-extension');
+    const userDataDir = testInfo.outputPath('test-user-data-dir');
+    browserContext = await chromium.launchPersistentContext(userDataDir, {
+      args: [
+        `--disable-extensions-except=${pathToExtension}`,
+        `--load-extension=${pathToExtension}`
+      ]
+    });
+  });
+
+  test.afterEach(async () => {
+    // Don't forget to close the created context.
+    // Closing persistent context will close the browser.
+    await browserContext.close();
+  });
+
+  test('Should get handle to background page of extension', async () => {
+    const backgroundPage = browserContext.backgroundPages()[0];
+    // Test the background page as you would any other page.
+  });
+});
