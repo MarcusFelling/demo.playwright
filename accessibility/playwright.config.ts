81a0f81e22d456df204b1b7afabb8e62ad484644
Marcus Felling <marcus.felling@microsoft.com>
2022-05-05 10:28:33 -0500

    update deps, add github reporter for annotations

--- accessibility/playwright.config.ts
+++ accessibility/playwright.config.ts
@@ -29,8 +29,10 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-
+  reporter: [
+    ['github'],
+    ['html'],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
 
