430e6cd50dbe32510b2cd5050d7874ab477ae9d4
Marcus Felling <marcus.felling@microsoft.com>
2022-09-12 10:51:09 -0500

    update accessibility tests

--- accessibility/tests/example.spec.ts
+++ accessibility/tests/example.spec.ts
@@ -1,38 +1,20 @@
-import { expect, test } from '@playwright/test';
-import { injectAxe, getViolations } from 'axe-playwright';
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
 test.describe.parallel('accessibility checks', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('');
+    await page.goto('https://playwright.dev/docs/accessibility-testing');
   });
 
-  /**
-   * In this test we use accessibility.snapshot() method to capture the current state of the accessibility tree
-   * @see https://playwright.dev/docs/api/class-accessibility
-   */
-  test('Dump the entire accessibility tree', async ({ page }) => {
-    const snapshot = await page.accessibility.snapshot();
-    console.log(snapshot);
-  });
-
-  /**
-   * In this test we use the Axe accessibility testing engine
-   * to run analysis on page
-   * @see https://github.com/abhinaba-ghosh/axe-playwright
-   */
-  test('Check entire page accessibility', async ({ page }) => {
-    // inject the axe-core runtime into the page under test
-    await injectAxe(page);
-
-    const violations = await getViolations(page, null, {
-      detailedReport: true,
-      axeOptions: {
-        runOnly: {
-          type: 'tag',
-          values: ['wcag2a', 'best-practice'], // all tags and standards listed here: https://www.deque.com/axe/core-documentation/api-documentation/#axe-core-tags
-        },
-      },
+/**
+ * This example demonstrates how to test an entire page for automatically detectable accessibility violations. 
+ * @see https://playwright.dev/docs/accessibility-testing
+ */
+test.describe('Scanning an entire page', () => {
+    test('should not have any automatically detectable accessibility issues', async ({ page }) => {
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+  
+      expect(accessibilityScanResults.violations).toEqual([]);
     });
-    expect(violations).toEqual([]);
-  });
+  });    
 });
