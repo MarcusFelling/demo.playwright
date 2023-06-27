beb7bda3aefd854d31cffc2478d2dc7c9f456111
Marcus Felling <marcus.felling@gmail.com>
2023-06-27 11:52:36 -0500

    only run vrt on chromium

--- visual-comparison/playwright.config.ts
+++ visual-comparison/playwright.config.ts
@@ -29,17 +29,5 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
       },
     },
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
   ],
 });
