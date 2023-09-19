5a05311cc4ec4b17784337d708014300ee5023f5
Marcus Felling <marcus.felling@gmail.com>
2023-09-19 10:42:58 -0500

    Update to 1.38.0, accessibility, and visual comparison tests (#40)
    * 1.38.0
    
    * update visual comparison example with new toHaveScreenshot function
    
    * update accessibility example

--- accessibility/tests/example.spec.ts
+++ accessibility/tests/example.spec.ts
@@ -9,11 +9,18 @@ test.describe('accessibility checks', () => {
    * This example demonstrates how to test an entire page for automatically detectable accessibility violations. 
    * @see https://playwright.dev/docs/accessibility-testing
    */
-  test.describe('Scanning an entire page', () => {
-    test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-      
-      expect(accessibilityScanResults.violations).toEqual([]);
-    });
+  test('should not have any automatically detectable accessibility issues', async ({ page }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+
+  test('should not have any automatically detectable WCAG A or AA violations', async ({ page }) => {
+
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+      .analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
