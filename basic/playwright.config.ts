38f826d37c9cda11bfcae2ca979bd899ec41b2ec
Marcus Felling <marcus.felling@gmail.com>
2024-08-12 16:46:44 +0000

    update basic test config

--- basic/playwright.config.ts
+++ basic/playwright.config.ts
@@ -29,29 +29,5 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
       },
     },
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
-    {
-      name: 'Mobile Chrome',
-      use: {
-        ...devices['Pixel 5'],
-      },
-    },
-    {
-      name: 'Mobile Safari',
-      use: {
-        ...devices['iPhone 12'],
-      },
-    },
   ],
 });
